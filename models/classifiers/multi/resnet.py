5d5c533ce6c3d83c936d5afc008a2692465f155c
minHnguyen0103 <minhhnguyen0312@gmail.com>
2023-02-09 17:28:57 +0800

    addcode

---
+++ models/classifiers/multi/resnet.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet18
+from ..base_model import BaseClassifierModel
+
+class ResnetBinary(BaseClassifierModel):
+    def __init__(self, config, opt_mod, sch_mod):
+        super(ResnetBinary, self).__init__(config, opt_mod, sch_mod)
+        self.model = resnet18()
+        self.num_classes = config['num_classes']
+        self.classifier = nn.Linear(1000, self.num_classes)
+        self.criterion = nn.CrossEntropyLoss()
+        self.initialize()
+
+    def forward(self, image):
+        x = self.model(image)
+        logits = self.classifier(x)
+        return logits
+    
