db4bf4d06920b995922cb56359f70f8236d113d8
minhhnguyen0312 <minhhnguyen0312@gmail.com>
2023-02-17 11:54:40 +0800

    add code

--- models/gans/model.py
+++ models/gans/model.py
@@ -1,7 +1,35 @@
 import torch
-import torch.nn
+import torch.nn as nn
 
 from ..model import BaseModel
+
+class SimpleGenerator(nn.Module):
+    def __init__(self):
+        super(SimpleGenerator, self).__init__()
+        self.fc1 = nn.Linear(100, 256)
+        self.fc2 = nn.Linear(256, 784)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        x = x.view(-1, 1, 28, 28)
+        return x
+
+
+# Define the discriminator network
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 1)
+
+    def forward(self, x):
+        x = x.view(-1, 784)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.sigmoid(self.fc3(x))
+        return x
 
 class BaseGAN(BaseModel):
     def __init__(self, config):
@@ -10,7 +38,8 @@ class BaseGAN(BaseModel):
         self.disc = self.build_disc()
     
     def build_generator(self):
-        pass
+        gen = SimpleGenerator()
+        return gen
 
     def build_discriminator(self):
         pass
@@ -26,6 +55,7 @@ class BaseGAN(BaseModel):
     def train_disc_step(self, batch):
         self.gen.eval()
         self.disc.train()
+        self.disc.optimizer.zero_grad()
         real = batch['image']
         b, w, h, c = real.shape
         real_label = torch.ones((b,))
@@ -36,8 +66,30 @@ class BaseGAN(BaseModel):
         fake_loss = self.get_disc_loss(fake, fake_label)
         total_loss = (real_loss + fake_loss) / 2
 
-        loss.
+        total_loss.backward()
+        self.disc_optimizer.step()
+    
+    def train_gen_step(self, batch):
+        self.gen.train()
+        self.disc.eval()
+        self.gen_optimizer.zero_grad()
+        real = batch['image']
+        b, w, h, c = real.shape
+        fake = self.sample(n_sample=b, cond=batch, grad=True)
+        fake_real_label = torch.ones((b,))
+        
+        gen_loss = self.get_disc_loss(fake, fake_real_label)
+        gen_loss.backward()
+        self.gen.optimizer.step()
 
-
+    def sample(self, n_sample, cond=None, grad=True):
+        noise = torch.randn(n_sample, 100, device=self.device)
+        fake = self.gen(noise)
+        return fake
+    
+    def get_disc_loss(self, x, y):
+        pred = self.disc(x)
+        return self.criterion(pred, y)
+    
     def eval_step(self, batch, step):
         pass
